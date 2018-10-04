4af966c6b9845e91c4c7659bac7f926429736c25
vnakos <vnakos@customedialabs.com>
2018-10-04 12:22:47 +0300

    Add validation on what the user types in

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import List from './List';
 
+const isOk  = /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i;
+
 export default class App extends Component {
   constructor(props) {
     super(props);
@@ -9,7 +11,8 @@ export default class App extends Component {
       term: '',
       items: [],
       editMode: false,
-      itemToEdit: null
+      itemToEdit: null,
+      hasError: false
     };
   }
 
@@ -19,9 +22,17 @@ export default class App extends Component {
 
   onSubmit = (event) => {
     event.preventDefault();
+    
+    if (!isOk.test(this.state.term)) {
+      this.setState({
+        hasError: true
+      });
+      return false;
+    }
     this.setState({
       term: '',
-      items: [...this.state.items, this.state.term]
+      items: [...this.state.items, this.state.term],
+      hasError: false
     });
   }
 
@@ -59,7 +70,8 @@ export default class App extends Component {
     return (
       <div>
         <form className="App" onSubmit={this.onSubmit}>
-          <input value={this.state.term} onChange={this.onChange}  />
+          <input value={this.state.term} onChange={this.onChange}  /><br/>
+          <span className={"error " + (this.state.hasError ? "show" : "hidden")}>Please enter a valid HEX color code.</span><br/>
           {
             !this.state.editMode ?
               <button>Add</button>
